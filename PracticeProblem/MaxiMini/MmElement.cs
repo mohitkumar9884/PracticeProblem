b68d767bfb044ff9e79cd568ea93d8a803920dc1
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-25 23:38:41 +0530

    [mohitkumar9884] creating/modifying program in MaxMin

---
+++ PracticeProblem/MaxiMini/MmElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem.MaxiMini
+{
+    internal class MmElement
+    {
+        public void MaximumMinimum()
+        {
+            // Example array
+            int[] array = { 500000,189459,354366,467543};
+
+            // Find maximum and minimum elements
+            int maxElement = FindMax(array);
+            int minElement = FindMin(array);
+
+            // Print the results
+            Console.WriteLine("Maximum element: " + maxElement);
+            Console.WriteLine("Minimum element: " + minElement);
+        }
+        public int FindMax(int[] arr)
+        {
+            int max = arr[0]; // Assume the first element as the maximum
+
+            // Iterate through the array to find the maximum element
+            foreach (int num in arr)
+            {
+                if (num > max)
+                    max = num;
+            }
+
+            return max;
+        }
+
+       public int FindMin(int[] arr)
+       {
+            int min = arr[0]; // Assume the first element as the minimum
+
+            // Iterate through the array to find the minimum element
+            foreach (int num in arr)
+            {
+                if (num < min)
+                    min = num;
+            }
+
+            return min;
+       }
+    }
+}
--- PracticeProblem/Program.cs
+++ PracticeProblem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PracticeProblem.DuplicateElements;
 using PracticeProblem.FrequencyElement;
+using PracticeProblem.MaxiMini;
 using PracticeProblem.UniqueElement;
 
 namespace PracticeProblem
@@ -10,7 +11,7 @@ namespace PracticeProblem
         {
             Console.WriteLine("Welcome to Practice problems.");
             Console.WriteLine("Please chose any One program from below options");
-            Console.WriteLine("\n1.Dupliment\n2.Uniquelement\n3.Frequelement");
+            Console.WriteLine("\n1.Dupliment\n2.Uniquelement\n3.Frequelement\n4.MaxiMini");
             Console.WriteLine(" ");
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -28,6 +29,10 @@ namespace PracticeProblem
                     Frequency frequency = new Frequency();
                     frequency.Frequelement();
                     break;
+                    case 4:
+                    MmElement mmElement = new MmElement();
+                    mmElement.MaximumMinimum();
+                    break; 
                     default:
                     Console.WriteLine("Invalid option.");
                     break;
