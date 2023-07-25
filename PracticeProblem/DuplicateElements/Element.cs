2406582a2d7f9e3d848fc6ed7943dcc5ff34168a
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-25 20:50:04 +0530

    [mohitkumar9884] creating/adding program in Duplicate

---
+++ PracticeProblem/DuplicateElements/Element.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem.DuplicateElements
+{
+    internal class Element
+    {
+        public void Dupliment()
+        {
+            int[] array = { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 8, 9, 0, 0, 0 };
+
+            int duplicatesCount = CountDuplicateElements(array);
+            Console.WriteLine("Total number of duplicate elements: " + duplicatesCount);
+        }
+
+        public int CountDuplicateElements(int[] arr)
+        {
+            int dupli = 0; // store duplicate elements
+            Dictionary<int, int> elemount = new Dictionary<int, int>(); // count of occurrences of elements in the array name of dictionary elemount
+
+            // Count the occurrences of elements
+            foreach (int num in arr)
+            {
+                if (elemount.ContainsKey(num))
+                    elemount[num]++;
+                else
+                    elemount[num] = 1;
+            }
+
+            // Count the total duplicates
+            foreach (int value in elemount.Values)
+            {
+                if (value > 1)
+                    dupli++;
+            }
+
+            return dupli;
+        }
+
+    }
+}
--- PracticeProblem/Program.cs
+++ PracticeProblem/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace PracticeProblem
+﻿using PracticeProblem.DuplicateElements;
+
+namespace PracticeProblem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Practice problems.");
+            Console.WriteLine("Please chose any One program from below options");
+            Console.WriteLine("\n1.Dupliment");
+            Console.WriteLine(" ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                    case 1:
+                    Element element = new Element();
+                    element.Dupliment();
+                    break;
+            }
         }
     }
 }
